d6066c7310da49214bbfd208bcb11900b4992f07
jamong-mini <jamong.mini@gmail.com>
2024-09-11 17:22:17 +0900

    feat: TravoApps 생성 및 연결

---
+++ README.md
@@ -0,0 +1,5 @@
+# TravoApps UiKit-Codebase
+
+***
+> iOS_5기 사전캠프 1조 프로젝트
+> UiKit Codebase 제작
---
+++ TravoApps/TravoApps/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TravoApps
+//
+//  Created by Jamong on 9/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
